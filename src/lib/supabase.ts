2834eb0e13147ca1910fbac4a8b4ba4100bedb7e
hsher2000 <40781024+hsher2000@users.noreply.github.com>
2026-07-22 11:04:29 +0800

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://vgolhuycgalilunxhvxf.supabase.co";
+
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnb2xodXljZ2FsaWx1bnhodnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NTc2NzAsImV4cCI6MjEwMDEzMzY3MH0.w4TG1tO_-RMF0XaLwF2m9zfiTOh8GCmL1TTljueTKU4";
+
 export const supabase = createClient(
-  const supabaseUrl = "https://vgolhuycgalilunxhvxf.supabase.co",
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnb2xodXljZ2FsaWx1bnhodnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NTc2NzAsImV4cCI6MjEwMDEzMzY3MH0.w4TG1tO_-RMF0XaLwF2m9zfiTOh8GCmL1TTljueTKU4";
+    supabaseUrl,
+    supabaseKey
 );
