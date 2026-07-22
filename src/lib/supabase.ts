bf9c7e0dae7f8a5862d59a1deb3e6bc1f7f94309
hsher2000 <40781024+hsher2000@users.noreply.github.com>
2026-07-22 10:59:23 +0800

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  const supabaseUrl = "https://vgolhuycgalilunxhvxf.supabase.co",
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnb2xodXljZ2FsaWx1bnhodnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NTc2NzAsImV4cCI6MjEwMDEzMzY3MH0.w4TG1tO_-RMF0XaLwF2m9zfiTOh8GCmL1TTljueTKU4";
 );
